7e76a16d32d11f2d04e2d4176417edd9ec9bc2fe
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-05-05 18:14:19 +0600

    logging with timer

--- my_py_pkg/my_py_pkg/my_node_oop.py
+++ my_py_pkg/my_py_pkg/my_node_oop.py
@@ -6,7 +6,14 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("py_test_oop")# pass the node name
 
-        self.get_logger().info("Hello world for oop")
+        # self.get_logger().info("Hello world for oop")
+
+        self.counter = 0
+        self.create_timer(1.0, self.callback)
+    
+    def callback(self):
+        self.get_logger().info(f"hello {self.counter}")
+        self.counter += 1
 
 def main(args=None):
     rclpy.init(args = args)
