cf9f320099ebe9ae1519803d81cbcc725804cedb
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-05-08 23:30:30 +0600

    created publisher

--- my_py_pkg/setup.py
+++ my_py_pkg/setup.py
@@ -21,7 +21,8 @@ setup(
     entry_points={
         'console_scripts': [
             "py_node = my_py_pkg.my_first_node:main",
-            "py_oop_node = my_py_pkg.my_node_oop:main"
+            "py_oop_node = my_py_pkg.my_node_oop:main",
+            "robot_news_station = my_py_pkg.publisher:main"
 
         ],
     },
